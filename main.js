d88a0bca8bb0ab275cf384068d25cf9f975d9faf
Potato Land <96445998+potatoland4492@users.noreply.github.com>
2022-03-02 14:12:31 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+let Store = {
+  cookies: '0',
+  clickpower: '1',
+  generator: '1',
+  addCookie: function () {
+    Store.cookies = parseInt(Store.cookies) + parseInt(Store.clickpower);
+  },
+  upgradeClickpower: function () {
+    if (parseInt(Store.cookies) < 50 * parseInt(Store.clickpower)){
+      alert(`You need ${parseInt(Store.clickpower * 50)} cookies to upgrade clickpower (${50 * parseInt(Store.clickpower) - parseInt(Store.cookies)} more).`)
+    } else {
+      Store.cookies = parseInt(Store.cookies) - 50 * parseInt(Store.clickpower);
+      Store.clickpower = parseInt(Store.clickpower) + 1;
+    }
+  },
+  upgradeGenerator: function () {
+    if (parseInt(Store.cookies) < 50 * parseInt(Store.generator)) {
+      alert(`You need ${parseInt(Store.generator * 50)} cookies to upgrade the generator (${50 * parseInt(Store.generator) - parseInt(Store.cookies)} more).`)
+    } else {
+      Store.cookies = parseInt(Store.cookies) - 50 * parseInt(Store.generator);
+      Store.generator = parseInt(Store.generator) + 1;
+    }
+  },
+};
+
+setInterval(function () {
+  document.getElementById('addcookie-btn').innerHTML = `<span class=\"code\">+${parseInt(Store.clickpower)}</span> <img class=\"cookie-img\" src=\"cookie.png\">`
+}, 1);
+setInterval(function () {
+  document.getElementById('cookiecount-span').innerHTML = Store.cookies + ' <img class=\"cookie-img\" src=\"cookie.png\">';
+}, 1);
+setInterval(function () {
+  document.getElementById('addclickpower-btn').innerHTML = 'Upgrade Clickpower (currently <span class=\"code\">Lvl ' + Store.clickpower + '</span>, costs <span class=\"code\">' + parseInt(Store.clickpower) * 50 + '</span> <img class=\"cookie-img\" src=\"cookie.png\"> to upgrade)';
+}, 1);
+setInterval(function () {
+  document.getElementById('addgenerator-btn').innerHTML = 'Upgrade Generator (currently <span class=\"code\">Lvl ' + Store.generator + '</span>, costs <span class=\"code\">' + parseInt(Store.generator) * 50 + '</span> <img class=\"cookie-img\" src=\"cookie.png\"> to upgrade)';
+}, 1);
+setInterval(function () {
+  Store.cookies = parseInt(Store.cookies) + (parseInt(Store.generator));
+}, 500);
