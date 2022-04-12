aa1be6d3b7acd9293ba5c90f6dfe9ae38ed032d7
potatoland4492 <potatoland4492@gmail.com>
2022-04-11 19:19:13 -0700

    Fix structure

--- docs/main.js
+++ docs/main.js
@@ -2,77 +2,88 @@
 // create class
 class PotatoStorage {
 	constructor () {
+		// init properties
 		this.potatoes = '0';
 		this.clickpower = '1';
 		this.farms = '1';
+		// if LocalStorage has something, retrieve
+		if (localStorage.getItem('potatoes') && localStorage.getItem('clickpower') && localStorage.getItem('farms')) {
+			this.potatoes = localStorage.getItem('potatoes');
+			this.clickpower = localStorage.getItem('clickpower');
+			this.farms = localStorage.getItem('farms');
+		}
 	}
-  costSlope = function (type) {
-    switch (type) {
-      case 'clickpower':
-        if (this.clickpower < 100) {
-          return 25;
-        } else if (this.clickpower < 1000) {
-          return 50;
-        } else if (this.clickpower < 10000) {
-          return 100;
-        } else if (this.clickpower < 1000000) {
-          return 10000;
-        } else if (this.clickpower < 1000000000) {
-          return 1000000;
-        } else if (this.clickpower < 1000000000000) {
-          return 1000000000;
-        }
-        break;
-      case 'farms':
-        if (this.farms + 1 < 100) {
-          return 25;
-        } if (this.farms + 1 < 1000) {
-          return 50;
-        } else if (this.farms + 1 < 10000) {
-          return 100;
-        } else if (this.farms + 1 < 1000000) {
-          return 10000;
-        } else if (this.farms + 1 < 1000000000) {
-          return 1000000;
-        } else if (this.farms + 1 < 1000000000000) {
-          return 1000000000;
-        }
-        break;
-      default:
-        return null;
-        break;
-    }
-  }
-  addPotato = function () {
-    this.potatoes = parseInt(this.potatoes) + parseInt(this.clickpower);
-  }
-  upgradeClickpower = function () {
-    if (parseInt(this.potatoes) < this.costSlope('clickpower') * parseInt(this.clickpower)) {
-      f.modal(`You need ${parseInt(this.clickpower * this.costSlope('clickpower'))} potatoes to upgrade clickpower (${this.costSlope('clickpower') * parseInt(this.clickpower) - parseInt(this.potatoes)} more).`)
-    } else {
-      this.potatoes = parseInt(this.potatoes) - this.costSlope('clickpower') * parseInt(this.clickpower);
-      this.clickpower = parseInt(this.clickpower) + 1;
-    }
-  }
-  addFarm = function () {
-    if (parseInt(this.potatoes) < this.costSlope('farms') * parseInt(this.farms)) {
-      f.modal(`You need ${parseInt(this.farms * this.costSlope('farms'))} potatoes to upgrade the farms (${this.costSlope('farms') * parseInt(this.farms) - parseInt(this.potatoes)} more).`);
-    } else {
-      this.potatoes = parseInt(this.potatoes) - this.costSlope('farms') * parseInt(this.farms);
-      this.farms = parseInt(this.farms) + 1;
+	// how much costs
+	calculateCost = function (type) {
+		switch (type) {
+			case 'clickpower':
+				if (parseInt(this.clickpower) < 100) {
+					return 25 * parseInt(this.clickpower);
+				} else if (parseInt(this.clickpower) < 1000) {
+					return 50 * parseInt(this.clickpower);
+				} else if (parseInt(this.clickpower) < 10000) {
+					return 100 * parseInt(this.clickpower);
+				} else if (parseInt(this.clickpower) < 1000000) {
+					return 10000 * parseInt(this.clickpower);
+				} else if (parseInt(this.clickpower) < 1000000000) {
+					return 1000000 * parseInt(this.clickpower);
+				} else if (parseInt(this.clickpower) < 1000000000000) {
+					return 1000000000 * parseInt(this.clickpower);
+				}
+				break;
+			case 'farms':
+				if (parseInt(this.farms) < 100) {
+					return 25 * parseInt(this.farms);
+				} if (parseInt(this.farms) < 1000) {
+					return 50 * parseInt(this.farms);
+				} else if (parseInt(this.farms) < 10000) {
+					return 100 * parseInt(this.farms);
+				} else if (parseInt(this.farms) < 1000000) {
+					return 10000 * parseInt(this.farms);
+				} else if (parseInt(this.farms) < 1000000000) {
+					return 1000000 * parseInt(this.farms);
+				} else if (parseInt(this.farms) < 1000000000000) {
+					return 1000000000 * parseInt(this.farms);
+				}
+				break;
+			default:
+				return null;
+		}
+	}
+	// self-explanatory
+	addPotato = function () {
+		this.potatoes = parseInt(this.potatoes) + parseInt(this.clickpower);
+	}
+	// self-explanatory
+	upgradeClickpower = function () {
+		if (parseInt(this.potatoes) < this.calculateCost('clickpower')) {
+			f.modal(`You need ${parseInt(this.calculateCost('clickpower'))} potatoes to upgrade clickpower (${this.calculateCost('clickpower') - parseInt(this.potatoes)} more).`)
+		} else {
+			this.potatoes = parseInt(this.potatoes) - this.calculateCost('clickpower');
+			this.clickpower = parseInt(this.clickpower) + 1;
+		}
+	}
+	// self-explanatory
+	addFarm = function () {
+		if (parseInt(this.potatoes) < this.calculateCost('farms')) {
+			f.modal(`You need ${parseInt(this.farms * this.calculateCost('farms'))} potatoes to upgrade the farms (${this.calculateCost('farms') - parseInt(this.potatoes)} more).`);
+		} else {
+			this.potatoes = parseInt(this.potatoes) - this.calculateCost('farms');
+			this.farms = parseInt(this.farms) + 1;
 			this.potatoes = 0;
 			this.clickpower = 1;
-    }
-  }
-  clearData = function () {
-    if (prompt('Are you sure? This will reset your progress! Type an UPPERCASE letter Y to clear.') === 'Y') {
-      clearInterval(saveInterval);
-      localStorage.clear();
-      location.reload();
-    } else {
-      alert('Aborted, data not cleared.');
-    }
-  }
+		}
+	}
+	// clear data from LocalStorage and reload
+	clearData = function () {
+		if (prompt('Are you sure? This will reset your progress! Type an UPPERCASE letter Y to clear.') === 'Y') {
+			clearInterval(saveInterval);
+			localStorage.clear();
+			location.reload();
+		} else {
+			alert('Aborted, data not cleared.');
+		}
+	}
 };
 // init objects
 let Store = new PotatoStorage();
@@ -80,11 +91,11 @@ let f = {
 	modal: function (message) {
 		let div = document.createElement('div');
 		div.style="zindex:1000000000000;color:#ffffff;font-size:1.75em;background-color:rgba(100,100,100,0.75);position:absolute;top:0;left:0;width:100vw;height:100vh;line-height:100vh;padding:0;margin:0;text-align:center;overflow-wrap:anywhere;";
-    div.innerText = message;
+		div.innerText = message;
 		document.body.appendChild(div);
-    setTimeout(function () {
-      document.body.removeChild(div);
-    }, 2500);
+		setTimeout(function () {
+			document.body.removeChild(div);
+		}, 2500);
 	},
 	wait: function (ms) {
 		let start = Date.now();
@@ -94,12 +105,6 @@ let f = {
 		}
 	}
 };
-// if LocalStorage has something, retrieve
-if (localStorage.getItem('potatoes') && localStorage.getItem('clickpower') && localStorage.getItem('farms')) {
-	Store.potatoes = localStorage.getItem('potatoes');
-	Store.clickpower = localStorage.getItem('clickpower');
-	Store.farms = localStorage.getItem('farms');
-}
 //#endregion classes and objects
 
 //#region intervals and event listeners
@@ -119,11 +124,11 @@ setInterval(function () {
 }, 1);
 // update contents of '#addclickpower-btn'
 setInterval(function () {
-	document.getElementById('addclickpower-btn').innerHTML = 'Upgrade Clickpower (currently <span class=\"code\">Lvl ' + Store.clickpower + '</span>, costs <span class=\"code\">' + parseInt(Store.clickpower) * Store.costSlope('clickpower') + '</span> <img class=\"potato-img\" src=\"potato.png\"> to upgrade)';
+	document.getElementById('addclickpower-btn').innerHTML = 'Upgrade Clickpower (currently <span class=\"code\">Lvl ' + Store.clickpower + '</span>, costs <span class=\"code\">' + parseInt(Store.clickpower) * Store.calculateCost('clickpower') + '</span> <img class=\"potato-img\" src=\"potato.png\"> to upgrade)';
 }, 1);
 
 setInterval(function () {
-	document.getElementById('addfarm-btn').innerHTML = 'Upgrade farms (currently <span class=\"code\">Lvl ' + Store.farms + '</span>, costs <span class=\"code\">' + parseInt(Store.farms) * Store.costSlope('farms') + '</span> <img class=\"potato-img\" src=\"potato.png\"> to upgrade)';
+	document.getElementById('addfarm-btn').innerHTML = 'Upgrade farms (currently <span class=\"code\">Lvl ' + Store.farms + '</span>, costs <span class=\"code\">' + parseInt(Store.farms) * Store.calculateCost('farms') + '</span> <img class=\"potato-img\" src=\"potato.png\"> to upgrade)';
 }, 1);
 
 setInterval(function () {
